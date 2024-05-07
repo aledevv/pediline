5c580dc30254d3c0d179d0a757f6d3b24f9868d2
aledevv <alessandro.devidi@studenti.unitn.it>
2024-05-07 12:27:14 +0200

    tolto codice inutile

--- app.js
+++ app.js
@@ -232,16 +232,6 @@ app.put('/api/v1/lines/:id', async (req, res) => { //modifica oggetto specifico
 /*****************************************************************************/
 
 
-const getProducts = async (req, res) => {
-    try {
-        const products = await Product.find();
-        res.status(200).json(products);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-};
-
-
 app.get('/', (req, res) => {
     res.send('Hello from PediLine!');
 });
---
+++ config.js
@@ -0,0 +1,2 @@
+const mongoDBToken = 'mongodb+srv://admin:pedilineCousCous69@cluster0.tq39k8r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+module.exports = mongoDBToken;
