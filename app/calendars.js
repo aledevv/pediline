6e83c32ee1cd5dad8ab68e4833a024aee619d1d5
enricotenuti <tenutz_27@outlook.it>
2024-05-12 22:04:20 +0200

    openapi stops,calendars,lists

--- app/calendars.js
+++ app/calendars.js
@@ -2,18 +2,76 @@ const express = require('express');
 const router = express.Router();
 const Calendar = require('./models/calendar');
 
+/**
+ * @openapi
+ * /api/v1/calendars:
+ *  get:
+ *    summary: Get all calendars
+ *    security:
+ *     - bearerAuth: []
+ *    responses:
+ *     200:
+ *      description: Success and returns all calendars
+ *      content:
+ *       application/json:
+ *        schema:
+ *         type: array
+ *         items:
+ *          $ref: '#/components/schemas/Calendar'
+ *        examples:
+ *         Calendars:
+ *          value:
+ *           - self: "/api/v1/calendars/1"
+ *             listePresenze: ["event1", "event2"]
+ *             bacheca: ["post1", "post2"]
+ *           - self: "/api/v1/calendars/2"
+ *             listePresenze: ["event3", "event4"]
+ *             bacheca: ["post3", "post4"]
+ *    tags:
+ *     - Calendars
+ */
 router.get('', async (req, res) => {
     let calendars = await Calendar.find({});
     calendars = calendars.map( (calendar) => {
         return {
             self: '/api/v1/calendars/' + calendar.id,
             listePresenze: calendar.listePresenze,
-	        bacheca: calendar.bacheca
+            bacheca: calendar.bacheca
         };
     });
     res.status(200).json(calendars);
 });
 
+/**
+ * @openapi
+ * /api/v1/calendars/{id}:
+ *  get:
+ *    summary: Get a calendar by ID
+ *    security:
+ *     - bearerAuth: []
+ *    parameters:
+ *     - in: path
+ *       name: id
+ *       required: true
+ *       description: ID of the calendar to get
+ *       schema:
+ *        type: string
+ *    responses:
+ *     200:
+ *      description: Success and returns the calendar
+ *      content:
+ *       application/json:
+ *        schema:
+ *         $ref: '#/components/schemas/Calendar'
+ *        examples:
+ *         CalendarExample:
+ *          value:
+ *           self: "/api/v1/calendars/1"
+ *           listePresenze: ["event1", "event2"]
+ *           bacheca: ["post1", "post2"]
+ *    tags:
+ *     - Calendars
+ */
 router.get('/:id', async (req, res) => {
     let calendar = await Calendar.findById(req.params.id);
     res.status(200).json({
@@ -23,34 +81,80 @@ router.get('/:id', async (req, res) => {
     });
 });
 
-// delete gestibile solamente dall'admin? da implementare
-router.delete('/:id', async (req, res) => {
-    let calendar = await Calendar.findById(req.params.id).exec();
-    if (!calendar) {
-        res.status(404).send()
-        console.log('calendar not found')
-        return;
-    }
-    await calendar.deleteOne()
-    console.log('calendar removed')
-    res.status(204).send()
-});
-
-
-// post gestibile solamente dall'admin? da implementare
+/**
+ * @openapi
+ * /api/v1/calendars/:
+ *  post:
+ *    summary: Creates a new calendar
+ *    security:
+ *     - bearerAuth: []
+ *    parameters:
+ *     - in: body
+ *       name: body
+ *       type: object
+ *       required: true
+ *       description: Calendar object
+ *       schema:
+ *        $ref: '#/components/schemas/Calendar'
+ *    responses:
+ *     201:
+ *      description: Success and returns the created calendar
+ *      content:
+ *       application/json:
+ *        schema:
+ *         $ref: '#/components/schemas/Calendar'
+ *        example:
+ *          listePresenze: ["event1", "event2"]
+ *          bacheca: ["post1", "post2"]
+ *    tags:
+ *     - Calendars
+ */
 router.post('', async (req, res) => {
 
 	let calendar = new Calendar({
         listePresenze: req.body.listePresenze,
-	    bacheca: req.body.bacheca
+        bacheca: req.body.bacheca
     });
     
 	calendar = await calendar.save();
-    let stopId = calendar.id;
+    let calendarId = calendar.id;
     console.log('Calendar saved successfully');
 
-    res.location("/api/v1/calendars/" + stopId).status(201).json(calendar).send();
+    res.location("/api/v1/calendars/" + calendarId).status(201).json(calendar).send();
 });
 
+/**
+ * @openapi
+ * /api/v1/calendars/{id}:
+ *  delete:
+ *    summary: Deletes a calendar by ID
+ *    security:
+ *     - bearerAuth: []
+ *    parameters:
+ *    - in: path
+ *      name: id
+ *      required: true
+ *      description: ID of the calendar to delete
+ *      schema:
+ *       type: string
+ *    responses:
+ *     204:
+ *      description: Success, calendar deleted
+ *     404:
+ *      description: Calendar not found
+ *    tags:
+ *     - Calendars
+ */
+router.delete('/:id', async (req, res) => {
+    let calendar = await Calendar.findById(req.params.id).exec();
+    if (!calendar) {
+        res.status(404).send()
+        console.log('Calendar not found')
+        return;
+    }
+    await calendar.deleteOne()
+    console.log('Calendar removed')
+    res.status(204).send()
+});
 
 module.exports = router;
--- app/lists.js
+++ app/lists.js
@@ -2,7 +2,38 @@ const express = require('express');
 const router = express.Router();
 const List = require('./models/list'); 
 
-
+/**
+ * @openapi
+ * /api/v1/lists:
+ *  get:
+ *    summary: Get all lists
+ *    security:
+ *     - bearerAuth: []
+ *    responses:
+ *     200:
+ *      description: Success and returns all lists
+ *      content:
+ *       application/json:
+ *        schema:
+ *         type: array
+ *         items:
+ *          $ref: '#/components/schemas/List'
+ *        examples:
+ *         Lists:
+ *          value:
+ *           - self: "/api/v1/lists/1"
+ *             day: "2024-05-12"
+ *             leaders: ["leader1", "leader2"]
+ *             studentsPresent: ["student1", "student2"]
+ *             studentsAbsent: ["student3", "student4"]
+ *           - self: "/api/v1/lists/2"
+ *             day: "2024-05-13"
+ *             leaders: ["leader3", "leader4"]
+ *             studentsPresent: ["student5", "student6"]
+ *             studentsAbsent: ["student7", "student8"]
+ *    tags:
+ *     - Lists
+ */
 router.get('', async (req, res) => {
     let lists = await List.find({});
     lists = lists.map( (list) => {
@@ -17,6 +48,38 @@ router.get('', async (req, res) => {
     res.status(200).json(lists);
 });
 
+/**
+ * @openapi
+ * /api/v1/lists/{id}:
+ *  get:
+ *    summary: Get a list by ID
+ *    security:
+ *     - bearerAuth: []
+ *    parameters:
+ *     - in: path
+ *       name: id
+ *       required: true
+ *       description: ID of the list to get
+ *       schema:
+ *        type: string
+ *    responses:
+ *     200:
+ *      description: Success and returns the list
+ *      content:
+ *       application/json:
+ *        schema:
+ *         $ref: '#/components/schemas/List'
+ *        examples:
+ *         ListExample:
+ *          value:
+ *           self: "/api/v1/lists/1"
+ *           day: "2024-05-12"
+ *           leaders: ["leader1", "leader2"]
+ *           studentsPresent: ["student1", "student2"]
+ *           studentsAbsent: ["student3", "student4"]
+ *    tags:
+ *     - Lists
+ */
 router.get('/:id', async (req, res) => {
     let list = await List.findById(req.params.id);
     res.status(200).json({
@@ -28,21 +91,36 @@ router.get('/:id', async (req, res) => {
     });
 });
 
-// delete gestibile solamente dall'admin? da implementare
-router.delete('/:id', async (req, res) => {
-    let list = await List.findById(req.params.id).exec();
-    if (!list) {
-        res.status(404).send()
-        console.log('list not found')
-        return;
-    }
-    await list.deleteOne()
-    console.log('list removed')
-    res.status(204).send()
-});
-
-
-// post gestibile solamente dall'admin? da implementare
+/**
+ * @openapi
+ * /api/v1/lists/:
+ *  post:
+ *    summary: Creates a new list
+ *    security:
+ *     - bearerAuth: []
+ *    parameters:
+ *     - in: body
+ *       name: body
+ *       type: object
+ *       required: true
+ *       description: List object
+ *       schema:
+ *        $ref: '#/components/schemas/List'
+ *    responses:
+ *     201:
+ *      description: Success and returns the created list
+ *      content:
+ *       application/json:
+ *        schema:
+ *         $ref: '#/components/schemas/List'
+ *        example:
+ *          day: "2024-05-12"
+ *          leaders: ["leader1", "leader2"]
+ *          studentsPresent: ["student1", "student2"]
+ *          studentsAbsent: ["student3", "student4"]
+ *    tags:
+ *     - Lists
+ */
 router.post('', async (req, res) => {
 
 	let list = new List({
@@ -54,10 +132,43 @@ router.post('', async (req, res) => {
     
 	list = await list.save();
     let listId = list.id;
-    console.log('list saved successfully');
+    console.log('List saved successfully');
 
     res.location("/api/v1/lists/" + listId).status(201).json(list).send();
 });
 
+/**
+ * @openapi
+ * /api/v1/lists/{id}:
+ *  delete:
+ *    summary: Deletes a list by ID
+ *    security:
+ *     - bearerAuth: []
+ *    parameters:
+ *    - in: path
+ *      name: id
+ *      required: true
+ *      description: ID of the list to delete
+ *      schema:
+ *       type: string
+ *    responses:
+ *     204:
+ *      description: Success, list deleted
+ *     404:
+ *      description: List not found
+ *    tags:
+ *     - Lists
+ */
+router.delete('/:id', async (req, res) => {
+    let list = await List.findById(req.params.id).exec();
+    if (!list) {
+        res.status(404).send()
+        console.log('List not found')
+        return;
+    }
+    await list.deleteOne()
+    console.log('List removed')
+    res.status(204).send()
+});
 
 module.exports = router;
