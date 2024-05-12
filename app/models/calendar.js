20ec047d24cb745d7a0afab171bc9c9e65be985f
enricotenuti <tenutz_27@outlook.it>
2024-05-12 22:17:14 +0200

    models aggiornati

--- app/models/calendar.js
+++ app/models/calendar.js
@@ -1,6 +1,28 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+/**
+ * @openapi
+ * components:
+ *  schemas:
+ *   Calendar:
+ *    type: object
+ *    properties:
+ *     listePresenze:
+ *      type: array
+ *      items:
+ *       type: string
+ *     bacheca:
+ *      type: string
+ *      format: uuid
+ *    required:
+ *     - listePresenze
+ *     - bacheca
+ *    example:
+ *      listePresenze: ["event1", "event2"]
+ *      bacheca: "bacheca1_ID"
+ */
+				
 // set up a mongoose model
 module.exports = mongoose.model('Calendar', new Schema({ 
 	listePresenze: Array,
--- app/models/stop.js
+++ app/models/stop.js
@@ -1,36 +1,34 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+/**
+ * @openapi
+ * components:
+ *  schemas:
+ *   Stop:
+ *    type: object
+ *    properties:
+ *     name:
+ *      type: string
+ *     schedule:
+ *      type: string
+ *     position:
+ *      type: array
+ *      items:
+ *       type: number
+ *    required:
+ *     - name
+ *     - schedule
+ *     - position
+ *    example:
+ *      name: "Stop 1"
+ *      schedule: "08:00 - 08:30"
+ *      position: [40.7128, -74.0060]
+ */
+				
 // set up a mongoose model
 module.exports = mongoose.model('Stop', new Schema({ 
 	name: String,
 	schedule: String,
     position: Array
 }));
-
-// previous version
-
-// const mongoose = require('mongoose');
-
-// const stopSchema = new mongoose.Schema(
-//     {
-//         name: {
-//             type: String,
-//             required: [true, 'Please enter school name'],
-//         },
-//         schedule: {
-//             type: String,
-//             required: [true, 'Please enter the schedule'],
-//         },
-//         positionId: {
-//             type: Array,
-//             required: [true, 'Please enter a valid position'],
-//         }
-//     },
-//     {
-//         timestamps: true
-//     },
-// );
-
-// const Fermata = mongoose.model('Fermata', stopSchema);
-// module.exports = Fermata;
