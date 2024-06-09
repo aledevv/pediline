7d70ebf8545831f40ab5bd5bef940f0c17ad7b9e
DanMa02 <daniele.marisa@studenti.unitn.it>
2024-06-10 00:05:03 +0200

    Spostato pannello admin

--- frontend/src/components/pauseButton.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <!-- PAUSE BUTTON -->
-    <v-col cols="auto">
-        <v-btn icon="mdi-pause" color="amber-accent-3" variant="flat"></v-btn> <span class="text-h6">Pausa</span>
-    </v-col>
-</template>
