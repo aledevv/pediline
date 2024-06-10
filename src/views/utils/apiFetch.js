38044953e5d060a7323c6b8064b1ce0c78028ff7
aledevv <alessandro.devidi@studenti.unitn.it>
2024-06-10 23:13:48 +0200

    modifiche api fetch per scuole

--- src/views/utils/apiFetch.js
+++ src/views/utils/apiFetch.js
@@ -1,5 +1,5 @@
 const HOST = 'http://localhost:3000/api/v1';
-
+import { setLoggedUser, loggedUser, getInfo } from '../../states/loggedUser.js';
 
 const API_ENDPOINTS = {
   LINE_URL: `${HOST}/lines`,
@@ -13,6 +13,8 @@ const API_ENDPOINTS = {
 
 // ----------------------- FETCH FUNCTIONS --------------------
 
+
+// ----------------------------------- SCHOOLS
 const fetchSchoolsFull = async () => {
   try {
     const response = await fetch(API_ENDPOINTS.SCHOOL_URL);
@@ -42,23 +44,70 @@ const fetchSchools = async () => {
     const response = await fetch(API_ENDPOINTS.SCHOOL_URL);
     const data = await response.json();
 
-    let schools = {};
+    // let schools = {};
 
-    for (let school of data) {
-      schools[school.name] = {
-        id: school.id,
-        name: school.name,
-        lines: school.linesId,
-        position: school.position
-      };
-    }
+    // for (let school of data) {
+    //   schools[school.name] = {
+    //     id: school.id,
+    //     name: school.name,
+    //     lines: school.linesId,
+    //     position: school.position
+    //   };
+    // }
 
-    return schools;
+    return data;
 
   } catch (error) {
     console.error('Error fetching data:', error);
   }
 };
+
+
+
+async function createSchool(school){
+  const response = await fetch(API_ENDPOINTS.SCHOOL_URL, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(school),
+  });
+  const data = await response.json();
+  console.log('School created:', data);
+  return data;
+}
+
+async function deleteSchool(id) {
+  try {
+      const response = await fetch(API_ENDPOINTS.SCHOOL_URL + `/${id}`, {
+          method: 'DELETE',
+      });
+
+      if (!response.ok) {
+          throw new Error('Errore durante l\'eliminazione della scuola');
+      }
+
+      // Ritorna un oggetto di successo
+      return { success: true, message: 'Scuola eliminata con successo' };
+  } catch (error) {
+      console.error('Errore durante l\'eliminazione della scuola:', error);
+      // Ritorna un oggetto di errore
+      return { success: false, message: 'Errore durante l\'eliminazione della scuola' };
+  }
+}
+
+async function updateSchool(id, body){
+  const response = await fetch(API_ENDPOINTS.SCHOOL_URL + `/${id}`, {
+      method: 'PUT',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+  });
+  const data = await response.json();
+  console.log('School updated:', data);
+  return data;
+}
 
 // ----------------------------------- TOKEN 
 
@@ -75,7 +124,7 @@ async function fetchToken() {
     }
 
     if (data.loggedUser) {
-      await getInfo(data);
+      await fetchUserInfo(data);
       console.log('Dati utente:', data.loggedUser);
     }
 
@@ -89,10 +138,11 @@ async function fetchToken() {
 async function fetchUserInfo(data) {
   const response = await fetch(API_ENDPOINTS.USER_URL + `?email=${data.loggedUser.email}`);
   const userInfo = await response.json();
-  console.log('User info:', userInfo);
+  //console.log('User info:', userInfo);
   setLoggedUser(userInfo[0]);
   loggedUser.token = data.token;
 }
+
 
 // ------------------------------------- STOPS AND LINES
 
@@ -118,11 +168,12 @@ async function fetchStopsByLines(lineList) {
   return linesArray;
 };
 
-async function fetchLines(lines) {
+async function fetchLines() {
   const response = await fetch(API_ENDPOINTS.LINE_URL);
   const data = await response.json();
-  lines.value = data.map(line => line.name);  // Assuming the API response is a list of objects with a 'name' property
-  console.log("Lines fetched:", lines.value);
+  //lines.value = data.map(line => line.name);  // Assuming the API response is a list of objects with a 'name' property
+  //console.log("Lines fetched:", lines.value);
+  return data;
 }
 
 async function fetchStops(stops) {
@@ -133,15 +184,15 @@ async function fetchStops(stops) {
 }
 
 async function fetchLineFromId(id){
-const response = await fetch(API_ENDPOINTS.LINE_URL + `?id=${id}`);
-const lineInfo = await response.json();
-return lineInfo[0];
+  const response = await fetch(API_ENDPOINTS.LINE_URL + `/${id}`);
+  const lineInfo = await response.json();
+  return lineInfo;
 }
 
 async function fetchStopFromId(id){
-const response = await fetch(API_ENDPOINTS.STOP_URL + `?id=${id}`);
-const stopInfo = await response.json();
-return stopInfo[0];
+  const response = await fetch(API_ENDPOINTS.STOP_URL + `/${id}`);
+  const stopInfo = await response.json();
+  return stopInfo;
 }
 
 async function fetchAlerts() {
@@ -152,37 +203,85 @@ async function fetchAlerts() {
 
 }
 
-async function createSchool(school){
-    const response = await fetch(API_ENDPOINTS.SCHOOL_URL, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(school),
-    });
-    const data = await response.json();
-    console.log('School created:', data);
-    return data;
+async function createLine(line){
+  const response = await fetch(API_ENDPOINTS.LINE_URL, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(line),
+  });
+  const data = await response.json();
+  console.log('Line created:', data);
+  return data;
 }
 
-async function deleteSchool(id) {
+async function deleteLine(id) {
   try {
-      const response = await fetch(API_ENDPOINTS.SCHOOL_URL + `/${id}`, {
+      const response = await fetch(API_ENDPOINTS.LINE_URL + `/${id}`, {
           method: 'DELETE',
       });
 
       if (!response.ok) {
-          throw new Error('Errore durante l\'eliminazione della scuola');
+          throw new Error('Errore durante l\'eliminazione della linea');
       }
 
       // Ritorna un oggetto di successo
-      return { success: true, message: 'Scuola eliminata con successo' };
+      return { success: true, message: 'Linea eliminata con successo' };
   } catch (error) {
-      console.error('Errore durante l\'eliminazione della scuola:', error);
+      console.error('Errore durante l\'eliminazione della linea:', error);
       // Ritorna un oggetto di errore
-      return { success: false, message: 'Errore durante l\'eliminazione della scuola' };
+      return { success: false, message: 'Errore durante l\'eliminazione della linea' };
   }
 }
 
+async function updateLine(id, body){
+  const response = await fetch(API_ENDPOINTS.LINE_URL + `/${id}`, {
+      method: 'PUT',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+  });
+  const data = await response.json();
+  //console.log('Line updated:', data);
+  return data;
+}
 
-export { fetchSchools, fetchSchoolsFull, fetchToken, fetchUserInfo, fetchStopsByLines, fetchLineFromId, fetchStopFromId, fetchStops, fetchLines, createSchool, deleteSchool, fetchAlerts};
+async function createStop(stop){
+  const response = await fetch(API_ENDPOINTS.STOP_URL, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(stop),
+  });
+  const data = await response.json();
+  //console.log('Stop created:', data);
+  return data;
+}
+
+async function deleteStop(id) {
+  try {
+      const response = await fetch(API_ENDPOINTS.STOP_URL + `/${id}`, {
+          method: 'DELETE',
+      });
+
+      if (!response.ok) {
+          throw new Error('Errore durante l\'eliminazione della fermata');
+      }
+
+      // Ritorna un oggetto di successo
+      return { success: true, message: 'Fermata eliminata con successo' };
+  } catch (error) {
+      console.error('Errore durante l\'eliminazione della fermata:', error);
+      // Ritorna un oggetto di errore
+      return { success: false, message: 'Errore durante l\'eliminazione della fermata' };
+  }
+}
+
+// ----------------------- EXPORT FUNCTIONS --------------------
+
+export { fetchSchools, fetchSchoolsFull, fetchToken, fetchUserInfo, fetchStopsByLines, fetchLineFromId, fetchStopFromId, 
+  fetchStops, fetchLines, createSchool, deleteSchool, fetchAlerts, createLine, updateSchool, deleteLine, 
+  updateLine, createStop, deleteStop, updateUserInfo };
